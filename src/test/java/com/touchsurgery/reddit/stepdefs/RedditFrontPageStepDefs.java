b19c7c9953e2b2cf0824a40b86c6b1dfa340960c
Priscilla Soares <psoares@thoughtworks.com>
2018-04-15 04:05:14 -0300

    Updates the packages and some classes name to better fit the context of the tests.

--- src/test/java/com/touchsurgery/reddit/stepdefs/RedditFrontPageStepDefs.java
+++ src/test/java/com/touchsurgery/reddit/stepdefs/RedditFrontPageStepDefs.java
@@ -7,7 +7,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class SearchStepDefs {
+public class RedditFrontPageStepDefs {
 
   private RedditFrontPage usingRedditSearchPage;
 
